af0f9d16f839b3fbc7ff0f2b0fa78482849f0e08
Will Naughton <willnaughton@Wills-MacBook-Pro.local>
2018-09-08 11:46:47 -0700

    getConnection method fix

--- src/dao/CharacterDao.java
+++ src/dao/CharacterDao.java
@@ -1,5 +1,18 @@
 package dao;
 
+import java.sql.Connection;
+
 public class CharacterDao {
 
+	
+	private Connection connection;
+	private final String GET_CHARACTER_BY_ID = "";
+	private final String GET_CHARACTER = "";
+	private final String DELETE_CHARACTER = "";
+	private final String CREATE_CHARACTER= "";
+	
+	public CharacterDao() {
+		connection = ConnectionManager.getConnection();
+
+	}
 }
--- src/dao/ConnectionManager.java
+++ src/dao/ConnectionManager.java
@@ -6,13 +6,13 @@ import java.sql.SQLException;
 
 public class ConnectionManager {
 	
-	private final static String URL = "jdbc:mysql://localhost:3306/promineo-magic";
+	private final static String URL = "jdbc:mysql://localhost:3306/characterMaker";
 	private final static String USERNAME = "magic";
 	private final static String PASSWORD = "12345";
 	private static Connection connection;
 	
 	
-	private static Connection getConnection() { 
+	public static Connection getConnection() { 
 		try {
 			if (connection == null) {
 				connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
