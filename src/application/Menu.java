ae7dead2ef1fe7214819dd074c82b61fe7ac9d99
keith <kmacdadie@example.com>
2018-09-09 20:33:12 -0700

    added dao and menu

--- src/application/Menu.java
+++ src/application/Menu.java
@@ -5,46 +5,100 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+import dao.VehiclesDao;
+import entity.Vehicles;
+
 public class Menu {
 	
+	private VehiclesDao vehiclesDao = new VehiclesDao();
 	private Scanner scanner = new Scanner(System.in);
-	private List<String> options = Arrays.asList(); //???? need menu options
-
+	private List<String> options = Arrays.asList(
+			"Display Vehicles",
+			"Display a Vehicle",
+			"Create Vehicle",
+			"Delete Vehicle",
+			"Update Vehicle");
+	
 	public void start() {
-		//user selects option from printed menu
 		String selection = "";
 		
-		//loop over menu so user can continue to choose
 		do {
 			printMenu();
 			selection = scanner.nextLine();
 			
 			try {
 				if (selection.equals("1")) {
-					//
+					displayVehicles();
 				} else if (selection.equals("2")) {
-					//
+					displayVehicle();
 				} else if (selection.equals("3")) {
-					//
+					createVehicle();
 				} else if (selection.equals("4")) {
-					//
+					deleteVehicle();
 				} else if (selection.equals("5")) {
-					//
-				} 
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			
-			
-			System.out.println("Press enter to continue: ");
-			scanner.nextLine();
-		} while (!selection.equals("-1"));
+					updateVehicle();
+					}
+				} catch (SQLException e) {
+				e.printStackTrace();	
+				}
+		
+			System.out.println("Press enter to continue..");
+			scanner.nextLine();			
+		} while(!selection.equals("-1"));
 	}
+	
 	private void printMenu() {
-		System.out.println("What would you like to do? Select an option:\n--------------------------------------");
+		System.out.println("Select an Option:\n-----------------------------");
 		for (int i = 0; i < options.size(); i++) {
-			//prints out each number for user choice
 			System.out.println(i + 1 + ") " + options.get(i));
+			
 		}
+	}
+	
+	private void displayVehicles() throws SQLException {
+		List<Vehicles> brands = VehiclesDao.getVehicles();
+		for (Vehicles vehicle : Vehilces) {
+			System.out.println(vehicle.getVehilceId() + ": " + vehicle.getVehicles());
+		}
+	}
+	
+	private void displayVehicle() throws SQLException {
+		System.out.print("Enter Vehicle id:");
+		int id = Integer.parseInt(scanner.nextLine());
+		Vehicles Vehicle = VehiclesDao.getVehiclesById(id);
+		System.out.println(Vehicles.getVehiclesId() + ": " + Vehicles.getVehicles());
+		
+		}
+
+	private void createNewVehicle() throws SQLException {
+		System.out.print("Enter new Vehicle name");
+		String vehicleName = scanner.nextLine();
+		VehiclesDao.createNewVehicle(vehicleName);
+
+	}
+	
+	private void deleteVehicleById() throws SQLException {
+		System.out.print("Enter vehicle id to delete:");
+		int id = Integer.parseInt(scanner.nextLine());
+		VehiclesDao.deleteVehicleById(id);
+	}
+	
+	private void updateVehicleById() throws SQLException {
+		System.out.print("Enter name of vehicle:");
+		String name = scanner.nextLine();
+		System.out.print("Enter type vehicle:");
+		String type = scanner.nextLine();
+		System.out.print("Enter speed of vehicle:");
+		int speed = Integer.parseInt(scanner.nextLine());
+		System.out.print("Enter color of vehicle:");
+		String color = scanner.nextLine();
+		System.out.print("Enter vehicleId:");
+		int vehicleId = Integer.parseInt(scanner.nextLine());
+		VehiclesDao.updateVehiclesId(name, type, speed, color, vehicleId);
 	}
 }
---
+++ src/dao/VehiclesDao.java
@@ -0,0 +1,70 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import entity.Vehicles;
+
+
+public class VehiclesDao {
+	
+	private Connection connection;
+	private final String GET_VEHICLE_BY_CLASS_ID_QUERY = "SELECT * FROM vehicles WHERE class_id = ?";
+	private final String DELETE_VEHICLE_BY_CLASS_ID_QUERY = "DELETE FROM vehicles WHERE class_id = ?";
+	private final String CREATE_NEW_VEHICLE_QUERY = "INSERT INTO vehicles(name, type, speed, color, class_id) VALUES(?,?,?,?,?)";
+	private final String DELETE_VEHICLE_BY_ID_QUERY = " DELETE FROM vehicles WHERE id = ?";
+	private final String UPDATE_VEHICLE_ID_QUERY = "UPDATE FROM vehicles WHERE id = ?";
+	
+	public VehiclesDao() {
+		connection = Connection.getConnection();
+	}
+	
+	public List<Vehicles> getVehiclesByClassId(int classId) throws SQLException {
+		PreparedStatement ps = connection.prepareStatement(GET_VEHICLE_BY_CLASS_ID_QUERY);
+		ps.setInt(1, classId);
+		ResultSet rs = ps.executeQuery();
+		List<Vehicles> vehicles = new ArrayList<Vehicles>();
+		
+		while (rs.next()) {
+			vehicles.add(new Vehicles(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getString(5)));
+		}
+		
+		return null;
+	}
+
+	public void createNewVehicle(String name, String type, int speed, String color, int classId) throws SQLException {
+		PreparedStatement ps = connection.prepareStatement(CREATE_NEW_VEHICLE_QUERY);
+		ps.setString(1, name);
+		ps.setString(2, type);
+		ps.setInt(3, speed);
+		ps.setString(4, color);
+		ps.setInt(5, classId);
+		ps.executeUpdate();
+				
+	}
+	
+	public void deleteVehiclesByClassId(int classId) throws SQLException {
+		PreparedStatement ps = connection.prepareStatement(DELETE_VEHICLE_BY_CLASS_ID_QUERY);
+		ps.setInt(1, classId);
+		ps.executeUpdate();
+	}
+	
+	public void deleteVehiclesById(int id) throws SQLException {
+		PreparedStatement ps = connection.prepareStatement(DELETE_VEHICLE_BY_ID_QUERY);
+		ps.setInt(1, id);
+	}
+	
+	public void updateVehicleById(String name, String type, int speed, String color, int classId) throws SQLException {
+		PreparedStatement ps = connection.prepareStatement(UPDATE_VEHICLE_ID_QUERY);
+		ps.setString(1, name);
+		ps.setString(2, type);
+		ps.setInt(3, speed);
+		ps.setString(4, color);
+		ps.setInt(5, classId);
+		ps.executeUpdate();
+	}
+}
